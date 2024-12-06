f77fcea1c540e642152ea8d92d64e2d2c8685ce6
Kevin R. <kramirez8808@gmail.com>
2024-12-06 13:58:52 -0600

    Minor fixes

--- lbd.proyecto/src/main/java/lbd/proyecto/controller/FacturaController.java
+++ lbd.proyecto/src/main/java/lbd/proyecto/controller/FacturaController.java
@@ -61,7 +61,7 @@ public class FacturaController {
 
         facturaService.insertFactura(factura, pedidoResult, estadoResult);
         
-        return "redirect:/facturas/ver";
+        return "redirect:/factura/ver";
 
     }
 
@@ -77,7 +77,7 @@ public class FacturaController {
         Factura factura = new Factura();
         factura.setIdFactura(idFactura);
         facturaService.inactivarFactura(factura);
-        return "redirect:/facturas/ver";
+        return "redirect:/factura/ver";
     }
 
 }
